0c3cd106cbead4cb2de6d6482006c8d602329ef3
KCVO1995 <943514589@qq.com>
2020-01-06 16:19:24 +0800

    美化一下

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ let style = document.querySelector('#style')
 let n = 0
 let s2 = ''
 let s = 
-`/*你好，我加小辉
+`/*你好，我叫小辉
 * 接下来我演示一下我的前端功底
 * 首先我要准备一个div*/
 #div1{
@@ -63,7 +63,7 @@ let step = () => {
             n = n + 1
             step()
         }
-    }, 50);
+    }, 5);
 }
 
 step()
